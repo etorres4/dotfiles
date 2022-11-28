8af506503ebf7223b93ee76b1094fc2c56289d95
Eric Torres <erictorres4@protonmail.com>
2022-11-27 18:02:10 -0600

    Update fzf version

--- zsh/.config/zsh/conf.d/fzf.zsh
+++ zsh/.config/zsh/conf.d/fzf.zsh
@@ -3,8 +3,8 @@
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/usr/local/Cellar/fzf/0.33.0/shell/completion.zsh"
+[[ $- == *i* ]] && source "/usr/local/Cellar/fzf/0.35.1/shell/completion.zsh"
 
 # Key bindings
 # ------------
-source "/usr/local/Cellar/fzf/0.33.0/shell/key-bindings.zsh"
+source "/usr/local/Cellar/fzf/0.35.1/shell/key-bindings.zsh"
